d222b62e2c14de15baece8f18da08104f61d3292
Tri Kusuma Faradila <106002600+stardai35@users.noreply.github.com>
2025-07-31 06:33:37 +0700

    Update Klasifikasi_Golongan_Darah.py

--- pages/Klasifikasi_Golongan_Darah.py
+++ pages/Klasifikasi_Golongan_Darah.py
@@ -39,7 +39,7 @@ with st.expander("🧭 Panduan Mandiri: Upload atau Ambil Gambar Sidik Jari", ex
 # Pastikan path ke model Anda benar
 try:
     fingerprint_model = load_model('fingerprint_detection_model.h5')  # Model dengan kelas 'y'/'n'
-    model = load_model('best_vgg16_model.h5')
+    model = load_model('best_vgg16_modelbaik.h5')
     class_names = ['A', 'AB', 'B', 'O']
 except Exception as e:
     st.error(f"Gagal memuat model: {e}. Pastikan file 'best_vgg16_model.h5' ada dan tidak rusak.")
